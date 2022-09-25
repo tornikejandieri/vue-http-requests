3d45041605afb2f71f81fb51a401e31ad107dc09
Tornike Jandieri <108745129+tornikejandieri@users.noreply.github.com>
2022-09-25 21:52:02 +0400

    added fetch

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import BaseCard from './components/UI/BaseCard.vue';
 import BaseButton from './components/UI/BaseButton.vue';
 import App from './App.vue';
 
- = createApp(App);
+const app = createApp(App);
 
 app.component('base-card', BaseCard);
 app.component('base-button', BaseButton);
